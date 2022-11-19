807ba7f768f32f95dc5a32354e6a634f0ab2368e
tlkbsnt <tlkbsntnpl@gmail.com>
2022-11-19 23:18:41 +0100

    This is movie data fetching application

---
+++ src/main/java/fr/epita/movieapp/MoviePyApplication.java
@@ -0,0 +1,13 @@
+package fr.epita.movieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePyApplication.class, args);
+	}
+
+}
